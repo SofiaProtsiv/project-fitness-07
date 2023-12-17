d47f0a037ed799f58d6d83a4ea6b252c948fbe88
Evgeniy <egeniygal@gmail.com>
2023-12-17 21:39:53 +0200

    Add data checking to auth-forn

--- src/js/auth-modal/auth-modal.js
+++ src/js/auth-modal/auth-modal.js
@@ -1,4 +1,11 @@
 import { createUser, signIn, signOut, db } from '../firebase-service';
+import isEmailRight from '../helpers/email-checker';
+
+const authFormMessages = {
+  name: document.querySelector('.js__auth-modal__name__message'),
+  email: document.querySelector('.js__auth-modal__email__message'),
+  password: document.querySelector('.js__auth-modal__password__message'),
+};
 
 const userButton = document.querySelector('.header__auth_btn');
 const authModal = document.querySelector('.authModal__backdrop');
@@ -10,7 +17,7 @@ const usernameFieldset = document.querySelector('.authForm__fieldset.username');
 const title = document.querySelector('.authForm__title');
 
 const headerGroup = document.querySelectorAll('.header__nav-authorized');
-const authLinkEl = document.querySelectorAll(".header__nav__item.auth")
+const authLinkEl = document.querySelectorAll('.header__nav__item.auth');
 const btnLogOut = document.querySelectorAll('.header__logout_btn');
 
 // const headerGroupMobile = document.querySelector(".mobile__nav-authorized")
@@ -41,6 +48,30 @@ const resetForm = () => {
 const handleSubmit = async event => {
   event.preventDefault();
 
+  if (
+    !authForm.elements.name.value ||
+    authForm.elements.name.value.length < 3
+  ) {
+    authFormMessages.name.classList.remove('hidden');
+    return;
+  }
+
+  if (
+    !authForm.elements.email.value ||
+    !isEmailRight(authForm.elements.email.value)
+  ) {
+    authFormMessages.email.classList.remove('hidden');
+    return;
+  }
+
+  if (
+    !authForm.elements.password.value ||
+    authForm.elements.password.value.length < 6
+  ) {
+    authFormMessages.password.classList.remove('hidden');
+    return;
+  }
+
   const formData = [...authForm.elements].reduce((formData, element) => {
     if (element.name) {
       formData[element.name] = element.value;
@@ -54,6 +85,7 @@ const handleSubmit = async event => {
 
   await signIn(formData);
   resetForm();
+  authForm.elements.regSubmitBtn.disabled = true;
   // const currentUserName = (await db.auth().currentUser)?.displayName;
   // userName.textContent = currentUserName;
   closeModal();
@@ -87,23 +119,23 @@ const checkCurrentUser = async () => {
       // userName.classList.remove('hidden');
       authLinkEl.forEach(el => {
         el.classList.add('hidden');
-      })
+      });
       btnLogOut.forEach(el => {
         el.classList.add('visible');
-      })
+      });
       headerGroup.forEach(el => {
         el.classList.add('visible');
-      })
+      });
     } else {
       authLinkEl.forEach(el => {
         el.classList.remove('hidden');
-      })
+      });
       btnLogOut.forEach(el => {
         el.classList.remove('visible');
-      })
+      });
       headerGroup.forEach(el => {
         el.classList.remove('visible');
-      })
+      });
     }
   });
 };
@@ -112,9 +144,9 @@ const handleSignOut = async () => {
   await signOut();
   btnLogOut.forEach(el => {
     el.classList.add('hidden');
-  })
+  });
 
-  window.location.pathname = '/project-fitness-07/'
+  window.location.pathname = '/project-fitness-07/';
   // userName.classList.add('hidden');
 };
 
@@ -123,7 +155,7 @@ authForm.addEventListener('submit', handleSubmit);
 btnChangeForm.addEventListener('click', changeForm);
 btnLogOut.forEach(el => {
   el.addEventListener('click', handleSignOut);
-})
+});
 
 document.addEventListener('keydown', event => {
   if (event.key === 'Escape') {
@@ -138,3 +170,35 @@ authModal.addEventListener('click', event => {
 });
 
 checkCurrentUser();
+
+authForm.elements.name.addEventListener('focus', handlerNameFocus);
+
+function handlerNameFocus() {
+  authFormMessages.name.classList.add('hidden');
+}
+
+authForm.elements.email.addEventListener('focus', handlerEmailFocus);
+
+function handlerEmailFocus() {
+  authFormMessages.email.classList.add('hidden');
+}
+
+authForm.elements.password.addEventListener('focus', handlerPasswordFocus);
+
+function handlerPasswordFocus() {
+  authFormMessages.password.classList.add('hidden');
+}
+
+authForm.elements.name.addEventListener('input', handlerInputData);
+
+authForm.elements.email.addEventListener('input', handlerInputData);
+
+authForm.elements.password.addEventListener('input', handlerInputData);
+
+function handlerInputData() {
+  authForm.elements.name.value &&
+  authForm.elements.email.value &&
+  authForm.elements.password.value
+    ? (authForm.elements.regSubmitBtn.disabled = false)
+    : (authForm.elements.regSubmitBtn.disabled = true);
+}
