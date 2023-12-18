e4a71adbaae3202e21e0522e3fc879da98fc2752
M. A. Startsev <m.a.startsev@gmail.com>
2023-12-18 16:24:26 +0200

    fix and del console

--- src/js/favoritesDB/toggleFavoriteStatus.js
+++ src/js/favoritesDB/toggleFavoriteStatus.js
@@ -8,19 +8,17 @@ export const toggleFavoriteStatus = async function (exercise) {
     if (isFavoriteValue) {
       // Видалити об'єкт зі списку улюблених
       const response = await favoritesDB.remove(_id);
-      console.log(`Object with _id ${_id} removed from favorites.`);
+
       localStorage.setItem('isDataOld', JSON.stringify(true));
       return false;
     } else {
       // Додати об'єкт на сервер та отримати відповідь
       localStorage.setItem('isDataOld', JSON.stringify(true));
       const response = await favoritesDB.add(_id, exercise);
-      console.log(`Object with _id ${_id} added to favorites.`);
 
       return response;
     }
   } catch (error) {
-    console.error(error);
     return false;
   }
 };
